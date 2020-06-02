83e4f27f5d4e9e72b229c61ac4bc01d2dda5c807
Rocket720 <Mohapatra06142002>
2020-06-01 20:21:36 -0400

    fixed a bunch of stuff. adding cards soon tm

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn FlaskDemo:app -b 0.0.0.0:$PORT
+web: gunicorn arnavSpace:app -b 0.0.0.0:$PORT
--- arnavSpace.py
+++ arnavSpace.py
@@ -16,6 +16,14 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == "__main__":
     #app.config['SERVER_NAME'] = 'arnav.space:5000'
     app.run(host='0.0.0.0', port=int(os.getenv("PORT", "8080")), debug=True)
